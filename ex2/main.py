[FIX] Fix center coordinates calculation

ex2/main.py
@@ -7,8 +7,8 @@ import sys
 sys.path.append('..')
 from utils import *
 
-THRESHOLD_RADIUS = 30
-MAX_ITER = 1000
+THRESHOLD_RADIUS = 100
+MAX_ITER = 10000
 MATCHES_N = 2
 
 class Model:
@@ -16,6 +16,9 @@ class Model:
         self.x = x
         self.y = y
         self.theta = theta
+
+    def __str__(self):
+        return f"{self.x} {self.y} {math.degrees(self.theta)}"
 
 def get_relevant_matches(frag_img, matches, img_keypoints, frag_keypoints, n=MATCHES_N, max_i=MAX_ITER, threshold_radius=THRESHOLD_RADIUS):
     # TODO: Initialize
@@ -50,7 +53,7 @@ def get_relevant_matches(frag_img, matches, img_keypoints, frag_keypoints, n=MAT
         r = get_rotation_matrix(theta)
         A_frag_rotated = np.matmul(r, np.array(A_frag))
         t = get_translation(A_img, A_frag_rotated)
-        center_img = get_translated_coordinates(t, center_frag)
+        center_img = get_translated_coordinates(center_frag, t, theta)
         
         model = Model(int(center_img[0]), int(center_img[1]), theta)
         models.append(model)
@@ -95,11 +98,12 @@ def get_intersection_angle(A_frag, B_frag, A_img, B_img):
 def get_translation(pt_img, pt_origin):
     return np.array([pt_img[0] - pt_origin[0], pt_img[1] - pt_origin[1]])
 
-def get_translated_coordinates(pt, t):
-    return (t[0] + pt[0], t[1] + pt[1])
+def get_translated_coordinates(pt, t, theta):
+    rotated_pt = np.matmul(get_rotation_matrix(theta), np.array(pt))
+    return (t[0] + rotated_pt[0], t[1] + rotated_pt[1])
 
 def get_rotation_matrix(theta):
-    """[summary]
+    """Get rotation matrix in a left-handed coordinate system
 
     Args:
         theta (double): Angle in radian
@@ -109,13 +113,39 @@ def get_rotation_matrix(theta):
     """
     return np.array(
         ( 
-            (np.cos(theta), -np.sin(theta)),
-            (np.sin(theta),  np.cos(theta)) )
+            (np.cos(theta), np.sin(theta)),
+            (-np.sin(theta),  np.cos(theta)) )
         )
 
 # TODO: Question 3
-def get_model_regression(inliers_matches):
-    pass
+
+def get_model(A_match, B_match, img_keypoints, frag_keypoints, cols_f, rows_f):
+    A_img, A_frag = get_match_keypoints_coords(A_match, img_keypoints, frag_keypoints)
+    B_img, B_frag = get_match_keypoints_coords(B_match, img_keypoints, frag_keypoints)
+    theta = get_intersection_angle(A_frag, B_frag, A_img, B_img)
+    center_frag = ((cols_f - 1) / 2.0, (rows_f - 1) / 2.0)
+    r = get_rotation_matrix(theta)
+    A_frag_rotated = np.matmul(r, np.array(A_frag))
+    t = get_translation(A_img, A_frag_rotated)
+    center_img = get_translated_coordinates(center_frag, t, theta)
+    
+    return Model(int(center_img[0]), int(center_img[1]), theta)
+
+def get_model_regression(frag_img, initial_model, inliers_matches, img_keypoints, frag_keypoints):
+    cols_f, rows_f = frag_img.shape[:2]
+    models = [initial_model]
+    for i in range(len(inliers_matches)):
+        model_matches = [inliers_matches[i]]
+        if i + 1 != len(inliers_matches) - 1:
+            for j in range(i + 1, len(inliers_matches)):
+                # TODO 
+                model_matches.append(inliers_matches[j])
+                model = get_model(model_matches[0], model_matches[1], img_keypoints, frag_keypoints, cols_f, rows_f)
+                models.append(model)
+    x_avg = sum(model.x for model in models)/float(len(models))
+    y_avg = sum(model.y for model in models)/float(len(models))
+    theta_avg = sum(model.theta for model in models)/float(len(models))
+    return Model(int(x_avg), int(y_avg), theta_avg)
 
 if __name__ == "__main__":
     # Get the fresco
@@ -148,19 +178,24 @@ if __name__ == "__main__":
         print("   fragment_kp: ", len(fragment_kp))
 
         if len(fragment_kp) < 2:
-            print("     ! Not enougth kp")
+            print("        ! Not enough keypoints")
             continue
 
         ok_frag_kp_nb += 1
 
         # Get the BFMatcher associations
         matches = get_BFMatcher_associations(fragment_des, fresco_des, 0.75)
-        print("   matches: ", len(matches))
+        print("        matches: ", len(matches))
         if len(matches) < 2:
-            print("     ! Not enough matches")
+            print("             ! Not enough matches")
             continue
 
         inliers_matches, best_model = get_relevant_matches(fragment, matches, fresco_kp, fragment_kp)
+        if len(inliers_matches) > MATCHES_N:
+            best_model = get_model_regression(fragment, best_model, inliers_matches, fresco_kp, fragment_kp)
+
+        print(f"        inliers matches: {len(inliers_matches)}")
+        print(f"        model: {best_model}")
 
         unchanged_fragment_img = cv.imread(fragment_path, cv.IMREAD_UNCHANGED)
         cols_f, rows_f = unchanged_fragment_img.shape[:2]
@@ -168,6 +203,19 @@ if __name__ == "__main__":
 
         add_fragment_to_target_image({"x": best_model.x, "y": best_model.y},
                                      rotated_fragment, background, background.shape[1], background.shape[0])
+        
+        
+
+        cv.putText(
+            background,
+            get_fragment_number(fragment_path),
+            (best_model.x, best_model.y),
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (255, 0, 0, 255),
+            2
+        )
+        
 
     cv.imshow(fragment_path, background)
     cv.waitKey()